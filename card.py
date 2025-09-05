20bbd999e78dc173f75526188008a04d911b20e4
ColdDrink <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2025-09-05 19:26:49 -0400

    Created Card and Criminal classes

---
+++ card.py
@@ -0,0 +1,19 @@
+class Card:
+    person1 = 0
+    money1 = 0
+    talk1 = 0
+    punch1 = 0
+    fuel1 = 0
+    special1 = 0
+
+    person2 = 0
+    money2 = 0
+    talk2 = 0
+    punch2 = 0
+    fuel2 = 0
+    special2 = 0
+
+    costToPurchase = 0
+    isBase = 0
+    costToRemove = 0
+    isInjury = 0
