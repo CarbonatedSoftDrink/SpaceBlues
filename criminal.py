4c97f2a37edaf360258a6b2a43ed1d6813f1be99
ColdDrink <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2025-09-05 19:34:32 -0400

    bruh

--- criminal.py
+++ criminal.py
@@ -1,10 +1,10 @@
 class Criminal:
     planet = 0
-    
+
     punchMax = 0
     punchCost = 0
     talkMax = 0
     talkCost = 0
 
     bonusOnDefeat = 0
-    refreshStore = 0
+    refreshStore = False
